5a948337b1a192880dce699cebab6a2874542cff
Yaacob Naftali (יעקב נפתלי) <mustafak@live.com.mx>
2023-04-09 18:44:20 -0600

    updated startup files

--- src/WebUI/Program.cs
+++ src/WebUI/Program.cs
@@ -1,59 +1,170 @@
 using TwitterClone.Infrastructure.Identity;
 using TwitterClone.Infrastructure.Persistence;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Threading.Tasks;
+using System.Linq;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+using NSwag;
+using NSwag.Generation.Processors.Security;
+using TwitterClone.Application;
+using TwitterClone.Application.Common.Interfaces;
+using TwitterClone.Infrastructure;
+using TwitterClone.WebUI.Configurations;
+using TwitterClone.WebUI.Filters;
+using TwitterClone.WebUI.Hubs;
+using TwitterClone.WebUI.Services;
 
-namespace TwitterClone.WebUI
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+
+
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructureServices(builder.Configuration, builder.Environment.IsDevelopment());
+
+builder.Services.TryAddEnumerable(
+    ServiceDescriptor.Singleton<IPostConfigureOptions<JwtBearerOptions>, 
+        ConfigureJwtBearerOptions>());
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddSingleton<ICurrentUserService, CurrentUserService>();
+builder.Services.AddTransient<IUserNotifierService, UserNotifierService>();
+
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddHealthChecks()
+    .AddDbContextCheck<ApplicationDbContext>();
+
+builder.Services.AddControllersWithViews(options =>
+        options.Filters.Add<ApiExceptionFilterAttribute>())
+    .AddFluentValidation();
+
+builder.Services.AddRazorPages();
+
+builder.Services.AddSignalR();
+
+// Customise default API behaviour
+builder.Services.Configure<ApiBehaviorOptions>(options =>
 {
-    public class Program
+    options.SuppressModelStateInvalidFilter = true;
+});
+
+// In production, the React files will be served from this directory
+builder.Services.AddSpaStaticFiles(configuration =>
+{
+    configuration.RootPath = "ClientApp/build";
+});
+
+builder.Services.AddOpenApiDocument(configure =>
+{
+    configure.Title = "TwitterClone API";
+    configure.AddSecurity("JWT", Enumerable.Empty<string>(), new OpenApiSecurityScheme
     {
-        public async static Task Main(string[] args)
+        Type = OpenApiSecuritySchemeType.ApiKey,
+        Name = "Authorization",
+        In = OpenApiSecurityApiKeyLocation.Header,
+        Description = "Type into the textbox: Bearer {your JWT token}."
+    });
+
+    configure.OperationProcessors.Add(new AspNetCoreOperationSecurityScopeProcessor("JWT"));
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    //force https in production behind heroku proxy
+    app.Use((context, next) => {
+        context.Request.Scheme = "https";
+        return next();
+    });
+
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHealthChecks("/health");
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseSpaStaticFiles();
+
+app.UseSwaggerUi3(settings =>
+{
+    settings.Path = "/api";
+    settings.DocumentPath = "/api/specification.json";
+});
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseIdentityServer();
+app.UseAuthorization();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller}/{action=Index}/{id?}");
+    endpoints.MapRazorPages();
+    endpoints.MapHub<NotificationsHub>("/hubs/notifications");
+});
+
+app.UseSpa(spa =>
+{
+    spa.Options.SourcePath = "ClientApp";
+
+    if (app.Environment.IsDevelopment())
+    {
+        spa.UseReactDevelopmentServer(npmScript: "start");
+    }
+});
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    try
+    {
+        var context = services.GetRequiredService<ApplicationDbContext>();
+
+        if (context.Database.IsNpgsql())
         {
-            var host = CreateHostBuilder(args).Build();
+            context.Database.Migrate();
+        }                   
 
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
+        var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+        var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
 
-                try
-                {
-                    var context = services.GetRequiredService<ApplicationDbContext>();
+        await ApplicationDbContextSeed.SeedDefaultUserAsync(userManager, roleManager);
+        await ApplicationDbContextSeed.SeedSampleDataAsync(context);
+    }
+    catch (Exception ex)
+    {
+        var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
 
-                    if (context.Database.IsNpgsql())
-                    {
-                        context.Database.Migrate();
-                    }                   
+        logger.LogError(ex, "An error occurred while migrating or seeding the database.");
 
-                    var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
-                    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
-
-                    await ApplicationDbContextSeed.SeedDefaultUserAsync(userManager, roleManager);
-                    await ApplicationDbContextSeed.SeedSampleDataAsync(context);
-                }
-                catch (Exception ex)
-                {
-                    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
-
-                    logger.LogError(ex, "An error occurred while migrating or seeding the database.");
-
-                    throw;
-                }
-            }
-
-            await host.RunAsync();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        throw;
     }
 }
+
+await app.RunAsync();
